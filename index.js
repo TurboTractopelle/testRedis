f253323cf4398368b30996889a03039cd409094a
Guillaume Drouen <gdrouen@siege.edpsciences.net>
2019-09-06 11:47:41 +0200

    feat: add env file

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const restify = require("restify");
 const PORT = 5000;
 const routes = require("./src/routes");
--- src/redisClient.js
+++ src/redisClient.js
@@ -1,4 +1,6 @@
 const redis = require("redis");
-const client = redis.createClient(6379, "redis");
+
+// const client = redis.createClient(6379, "redis");
+const client = redis.createClient(process.env.REDIS_PORT, process.env.REDIS_HOST);
 
 module.exports = client;
